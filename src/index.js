3f0f5b2e815bfcaf39b3c41898c3839f7e61b13c
Viviane Huguette <kemshuggs@gmail.com>
2025-02-06 20:08:01 -0500

    connected to the mongoDB

--- src/index.js
+++ src/index.js
@@ -3,19 +3,52 @@
 
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const cookoieParser = require('cookie-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+ require("dotenv").config();
 
 
-const port = process.env.PORT || 5000;
+
 const app = express();
-
-
 app.use(cors());
 app.use(express.json());
+app.use(cookoieParser());
+app.use(helmet());
+
+
+
+
+mongoose.connect("mongodb+srv://kemshuggs:Mypassword2025@backenddb.ffddu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+  .then(() => {
+    console.log("Connected to dataBase!");
+    // Launch server
+    app.listen(port, () => {
+      console.log(`Server is running on port 5000`);
+    });
+  })
+  .catch(() => {
+    console.log("connection failed!");
+  });
+
+const port = process.env.PORT || 5000;
+  
+
+
 
 app.get('/', (req, res) => {
-  res.json('message: Welcome to the API');
+  res.json("Welcome to the API");
 });
+
+
+
+
+
+
+
+
+
+
 
 
    const users = require('./Routers/users');
@@ -28,7 +61,3 @@ app.get('/', (req, res) => {
    app.use('/uplaod', uplaodRoutes);
 
 
-// Launch server
-app.listen(port, () => {
-    console.log(`Server is online!`);
-  });
