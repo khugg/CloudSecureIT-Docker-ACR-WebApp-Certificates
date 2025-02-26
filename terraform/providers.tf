550ee754410e5dcd0714160542f79102f19191a9
Viviane Huguette <kemshuggs@gmail.com>
2025-02-26 05:23:47 -0500

    created  providers.tf file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,19 @@
+    # 1. Specify the version of the AzureRM Provider to use
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=4.18.0"
+    }
+  }
+}
+
+
+# 2.configure Microsoft Azure Provider
+
+
+provider "azurerm" {
+  features {}
+ }
+    
